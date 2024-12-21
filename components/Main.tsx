9f0addb254ee2c73818c7890e45644ea223a273b
renurao1306 <renurao.1306@gmail.com>
2024-12-21 12:36:28 +0530

    added contact me button for main section

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import { FiSend } from 'react-icons/fi'
 
 const Main = () => {
     return (
@@ -29,6 +30,18 @@ const Main = () => {
                     <p className='text-blue-100 text-md'>
                         A sofware developer based in India
                     </p>
+
+
+                    <button className="mt-10 relative inline-flex h-12 w-100 overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-1 focus:ring-slate-200">
+                        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                            Contact Me!
+
+                            <span className='px-2'><FiSend /></span>
+
+                        </span>
+                    </button>
+
                 </div>
             </div>
 
