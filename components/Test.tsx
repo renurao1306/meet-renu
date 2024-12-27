fae0cad76e247311f4c5b4c4aaeebc33e4ce94a2
renurao1306 <renurao.1306@gmail.com>
2024-12-28 00:07:01 +0530

    basic FeatureSection added

--- components/Test.tsx
+++ components/Test.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { BackgroundGradient } from "./ui/BackgroundGradient";
-import { FlipWords } from "./ui/FlipWords";
-
-const words = ["Developer", "Innovator", "Coder", "Engineer", "Problem Solver", "Technophile"];
-
+import { FeaturesSectionDemo } from "./ui/FeatureSection";
 
 const Test = () => {
   return (
-    <div className="text-white-100 text-center text-3xl relative mt-20 mb-10">Meet the
-      <FlipWords words={words} duration={2000} className="text-2xl font-bold" />in Me!</div>
-
+    <FeaturesSectionDemo />
   );
 };
 
---
+++ components/ui/FeatureSection.tsx
@@ -0,0 +1,105 @@
+'use client';
+import React from "react";
+import { cn } from "@/lib/utils";
+import { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+import { IconBrandYoutubeFilled } from "@tabler/icons-react";
+import Link from "next/link";
+
+export function FeaturesSectionDemo() {
+  const features = [
+    {
+      title: "Track issues effectively",
+      description:
+        "Track and manage your project issues with ease using our intuitive interface.",
+      skeleton: <div className="flex justify-center"><img src="/flower.jpg" className="w-[80vh] border border-white-100 rounded-md"/></div>,
+      className:
+        "col-span-1 lg:col-span-4 border-b lg:border-r dark:border-neutral-800",
+    },
+    {
+      title: "Capture pictures with AI",
+      description:
+        "Capture stunning photos effortlessly using our advanced AI technology.",
+        skeleton: <div className="flex justify-center"><img src="/flower.jpg" className="w-[80vh] h-[40vh] border border-white-100 rounded-md"/></div>,
+      className: "border-b col-span-1 lg:col-span-2 dark:border-neutral-800",
+    },
+    {
+      title: "Watch our AI on YouTube",
+      description:
+        "Whether its you or Tyler Durden, you can get to know about our product on YouTube",
+        skeleton: <div className="flex justify-center"><img src="/flower.jpg" className="w-[80vh] border border-white-100 rounded-md"/></div>,
+      className:
+        "col-span-1 lg:col-span-3 lg:border-r  dark:border-neutral-800",
+    },
+    {
+      title: "Deploy in seconds",
+      description:
+        "With our blazing fast, state of the art, cutting edge, we are so back cloud servies (read AWS) - you can deploy your model in seconds.",
+        skeleton: <div className="flex justify-center"><img src="/flower.jpg" className="w-[80vh] border border-white-100 rounded-md"/></div>,
+      className: "col-span-1 lg:col-span-3 border-b lg:border-none",
+    },
+  ];
+  return (
+    <div className="relative z-20 py-10 lg:py-40 max-w-7xl mx-auto">
+      <div className="px-8">
+        <h4 className="text-3xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium text-black dark:text-white">
+          Packed with thousands of features
+        </h4>
+
+        <p className="text-sm lg:text-base  max-w-2xl  my-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
+          From Image generation to video generation, Everything AI has APIs for
+          literally everything. It can even create this website copy for you.
+        </p>
+      </div>
+
+      <div className="relative ">
+        <div className="grid grid-cols-1 lg:grid-cols-6 mt-12 xl:border rounded-md dark:border-neutral-800">
+          {features.map((feature) => (
+            <FeatureCard key={feature.title} className={feature.className}>
+              <FeatureTitle>{feature.title}</FeatureTitle>
+              <FeatureDescription>{feature.description}</FeatureDescription>
+              <div className=" h-full w-full">{feature.skeleton}</div>
+            </FeatureCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const FeatureCard = ({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn(`p-4 sm:p-8 relative overflow-hidden`, className)}>
+      {children}
+    </div>
+  );
+};
+
+const FeatureTitle = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <p className=" max-w-5xl mx-auto text-left tracking-tight text-black dark:text-white text-xl md:text-2xl md:leading-snug">
+      {children}
+    </p>
+  );
+};
+
+const FeatureDescription = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <p
+      className={cn(
+        "text-sm md:text-base  max-w-4xl text-left mx-auto",
+        "text-neutral-500 text-center font-normal dark:text-neutral-300",
+        "text-left max-w-sm mx-0 md:text-sm my-2"
+      )}
+    >
+      {children}
+    </p>
+  );
+};
+
