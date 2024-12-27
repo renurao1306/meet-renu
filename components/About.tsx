cb5fe768dd79fc40b42228d06ef71f4b9394859d
renurao1306 <renurao.1306@gmail.com>
2024-12-27 21:28:29 +0530

    updated font size in About component

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ const words = ["Developer", "Innovator", "Designer", "Engineer", "Creator", "Exp
 const About = () => {
   return (
     <>
-    <div className="text-white-100 text-center text-3xl relative mt-20 mb-10">Meet the <FlipWords words={words} duration={2000} className="text-2xl font-bold" />in Me!</div>
+    <div className="text-white-100 text-center text-3xl relative mt-20 mb-10">Meet the <FlipWords words={words} duration={2000} className="text-3xl font-bold" />in Me!</div>
       <ProfileCard />
     </>
   );
