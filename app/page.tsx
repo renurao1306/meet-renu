9d8673e884fdb1536e85ac1bdf5d48c130400f29
renurao1306 <renurao.1306@gmail.com>
2024-12-30 23:28:27 +0530

    updated FloatingNav to include Projects link; modified Projects component with new titles, descriptions, and adjusted image sizes

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }, { name: 'About', link: '/' }]} />
+        <FloatingNav navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }, {name: 'Projects', link: '/'} ,{ name: 'About', link: '/' }]} />
         <Main />
         <Projects />
         <About />
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,38 +7,47 @@ function Projects() {
       title: "Exploring AI & ML",
       description:
         "Effortlessly Create Perfect Schedules for schools & colleges with the Power of AI",
-      skeleton: <div className="flex justify-center"><img src="/timetable-landscape.jpeg" className="w-[100vh] h-[50vh] border border-white-100 rounded-md" /></div>,
+      skeleton: <div className="flex justify-center"><img src="/timetable-landscape.jpeg" className="w-[100vh] h-[70vh] border border-white-100 rounded-md" /></div>,
       className:
         "col-span-1 lg:col-span-4 border-b lg:border-r dark:border-neutral-800",
       showButton: true,
       buttonLabel: "Check it out on GitHub",
-      onClickButton: () => {window.open('https://github.com/renurao1306/college-timetabling-app-optapy', '_blank')}
+      onClickButton: () => { window.open('https://github.com/renurao1306/college-timetabling-app-optapy', '_blank') }
     },
     {
-      title: "Capture pictures with AI",
+      title: "Unlock the Magic",
       description:
-        "Capture stunning photos effortlessly using our advanced AI technology.",
-      skeleton: <div className="flex justify-center"><img src="/flower.jpg" className="w-[80vh] h-[40vh] border border-white-100 rounded-md" /></div>,
+        "Test your wizarding skills with Quizzly Hallows built using Flutter and Dart programming language",
+      skeleton: <div className="flex justify-center"><img src="/quizzly-hallows1.jpg" className="w-[80vh] h-[70vh] border border-white-100 rounded-md" /></div>,
       className: "border-b col-span-1 lg:col-span-2 dark:border-neutral-800",
+      showButton: true,
+      buttonLabel: "Check it out on GitHub",
+      onClickButton: () => { window.open('https://github.com/renurao1306/quizly-hallows.git', '_blank') }
     },
     {
-      title: "Watch our AI on YouTube",
+      title: "Turning Data into Discovery",
       description:
-        "Whether its you or Tyler Durden, you can get to know about our product on YouTube",
+        "Visualizing Trends and Unlocking Patterns for Deeper Insights using Python libraries",
       skeleton: <div className="flex justify-center"><img src="/flower.jpg" className="w-[80vh] border border-white-100 rounded-md" /></div>,
       className:
         "col-span-1 lg:col-span-3 lg:border-r  dark:border-neutral-800",
+      showButton: true,
+      buttonLabel: "Check it out on GitHub",
+      onClickButton: () => { window.open('https://github.com/renurao1306/SuicideRates-Data-Analysis.git', '_blank') }
     },
     {
-      title: "Deploy in seconds",
+      title: "The Novel Nest",
       description:
-        "With our blazing fast, state of the art, cutting edge, we are so back cloud servies (read AWS) - you can deploy your model in seconds.",
+        "A Full-Stack Bookstore Experience for Modern Readers",
       skeleton: <div className="flex justify-center"><img src="/flower.jpg" className="w-[80vh] border border-white-100 rounded-md" /></div>,
       className: "col-span-1 lg:col-span-3 border-b lg:border-none",
+      showButton: true,
+      buttonLabel: "Check it out on GitHub",
+      onClickButton: () => { window.open('https://github.com/renurao1306/TheNovelNest.git', '_blank') }
     },
   ];
   return (
-    <div className="relative z-20 py-10 lg:py-40 max-w-7xl mx-auto">
+    <div className="relative z-20 py-10 max-w-7xl mx-auto">
       <div className="px-8">
         <h4 className="text-3xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium text-black dark:text-white">
           Turning Concepts into Creations
